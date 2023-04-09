fe5a9b57b65af90a38f3c7603c3748d0c350340b
David Yoon <qqpo12@naver.com>
2023-04-09 17:28:26 +0900

    Initial Commit

---
+++ ExchangeApp/ExchangeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeAppApp.swift
+//  ExchangeApp
+//
+//  Created by Jiwon Yoon on 2023/04/09.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
